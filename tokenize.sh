cd9c6e08acc200b551e6f05e79396ca2027fffda
antonrykachevskiy <anton.rykachevskiy@yandex.ru>
2018-01-30 21:44:34 +0300

    faster tokenizer, proc inp

--- tokenize.sh
+++ tokenize.sh
@@ -4,9 +4,10 @@ home=/home/anton/deephack/onsight
 mosesdecoder=$home/libs/mosesdecoder
 subword_nmt=$home/libs/subword-nmt
 scripts=$home/scripts
-data=$home/data
+data=$home/test_data
 
 iters=32000
+threads=4
 
 cat $data/parallel_corpus.txt | $scripts/split_parallel.py -o $data
 
@@ -17,7 +18,7 @@ do
 		echo "Tokenizing ${corp}${lang} ..."
 		cat $data/$corp$lang.txt | \
 		$mosesdecoder/scripts/tokenizer/normalize-punctuation.perl | \
-		$mosesdecoder/scripts/tokenizer/tokenizer.perl -penn > \
+		$mosesdecoder/scripts/tokenizer/tokenizer.perl -threads=$threads -penn > \
 		$data/tok_$corp$lang.txt
 	done
 	(cat $data/tok_parallel$lang.txt; echo ""; cat $data/tok_corpus$lang.txt) > $data/all_tok_$lang.txt
@@ -35,10 +36,17 @@ do
     for corp in parallel corpus
 	do 
 		echo "Transforming ${corp} ${lang} ..."
-		cat $data/$corp$lang.txt | \
-		$mosesdecoder/scripts/tokenizer/normalize-punctuation.perl | \
-		$mosesdecoder/scripts/tokenizer/tokenizer.perl -penn | \
+		cat $data/tok_$corp$lang.txt | \
 		$subword_nmt/apply_bpe.py -c $data/$lang.bpe --vocabulary $data/$lang.voc \
 		--vocabulary-threshold 0 -o $data/bpe_$corp$lang.txt
 	done
 done
+
+cat $data/input.txt | \
+$mosesdecoder/scripts/tokenizer/normalize-punctuation.perl | \
+$mosesdecoder/scripts/tokenizer/tokenizer.perl -threads=$threads -penn > \
+$data/tok_input.txt
+
+cat $data/tok_input.txt | \
+$subword_nmt/apply_bpe.py -c $data/$lang.bpe --vocabulary $data/$lang.voc \
+--vocabulary-threshold 0 -o $data/bpe_input.txt
