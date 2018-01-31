de7bde3d20882ca368b09d7e30b13a5271c4f2c0
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-01-31 03:17:18 +0300

    refactor: style changes

--- batch_iterator.py
+++ batch_iterator.py
@@ -3,26 +3,24 @@ import numpy as np
 def iterate_minibatches(*to_split, **kwargs):
     """
         generates batches from the data, passed as first arguments
-        
+
         arguments:
         -batchsize: the number of rows in a batch
         -shuffle: if True shuffles the data and yields shuffled batches
     """
-    batchsize=1
-    shuffle=False
-    if 'batchsize' in kwargs:
-        batchsize = kwargs['batchsize']
-    
-    if 'shuffle' in kwargs:
-        shuffle = kwargs['shuffle']
+    batchsize = kwargs.get('batchsize', 1)
+    shuffle = kwargs.get('shuffle', False)
 
     res = [np.array(x) for x in to_split]
     size = res[0].shape[0]
+
     for x in res:
         assert x.shape[0] == size
+
     if shuffle:
         indices = np.arange(size)
         np.random.shuffle(indices)
+
     for start_idx in range(0, size, batchsize):
         if shuffle:
             excerpt = indices[start_idx:start_idx + batchsize]
