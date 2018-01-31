451f327a2cdceeb7ff44403cc9ce06cdecab23a4
antonrykachevskiy <anton.rykachevskiy@yandex.ru>
2018-01-31 16:52:48 +0300

    minor

--- run.sh
+++ run.sh
@@ -11,11 +11,11 @@ mkdir /nmt/data
 # Training the model
 PYTHONPATH=/nmt python3.6 /nmt/src/train.py gnmt \
             --data_path="$DATA_PATH" \
-            --batch_size=64 \
+            --batch_size=2 \
             --hp_file=/nmt/hp_files/gnmt.json \
             --gpu_memory_fraction=1 \
             --validate_every=100 \
-            --save_every=100 \
+            --save_every=2 \
             --max_epochs=10 \
             --use_early_stopping=True \
             --early_stopping_last_n=10
@@ -28,4 +28,4 @@ PYTHONPATH=/nmt python3.6 /nmt/src/run.py gnmt \
             --output_path="$OUTPUT_DATA_PATH/output.txt" \
             --gpu_memory_fraction=1 \
             --hp_file=/nmt/hp_files/gnmt.json \
-            --run_batch_size=64
+            --run_batch_size=2
