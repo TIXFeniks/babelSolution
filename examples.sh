52b377d50ec906183507f676c32408399ed17b79
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-01-31 03:50:59 +0300

    feat: extend example command

--- examples.sh
+++ examples.sh
@@ -1,1 +1,8 @@
-PYTHONPATH=. python src/train.py gnmt --data_path=data --batch_size=4
+PYTHONPATH=. python src/train.py gnmt \
+            --data_path=data \
+            --batch_size=4 \
+            --hp_file=hp_files/gnmt.json \
+            --gpu_memory_fraction=0.3 \
+            --validate_every=100 \
+            --val_split_size=0.1 \
+            --save_every=100
