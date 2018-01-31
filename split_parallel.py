c523c9dbdcd920ab25eaa84f38b68c19cc927825
justheuristic <justheuristic@gmail.com>
2018-01-31 20:28:10 +0300

    fix \n

--- split_parallel.py
+++ split_parallel.py
@@ -27,8 +27,8 @@ lines_val = lines[round(lines_num * (1 - args.fraction)):]
 with open(args.out_path + '/parallel_train1.txt', 'wb') as f:
     f.write('\n'.join([x.split('\t')[0] for x in lines_train]).encode('utf-8'))
 with open(args.out_path + '/parallel_train2.txt', 'wb') as f:
-    f.write(''.join([x.split('\t')[1] for x in lines_train]).encode('utf-8'))
+    f.write('\n'.join([x.split('\t')[1] for x in lines_train]).encode('utf-8'))
 with open(args.out_path + '/parallel_val1.txt', 'wb') as f:
     f.write('\n'.join([x.split('\t')[0] for x in lines_val]).encode('utf-8'))
 with open(args.out_path + '/parallel_val2.txt', 'wb') as f:
-    f.write(''.join([x.split('\t')[1] for x in lines_val]).encode('utf-8'))
+    f.write('\n'.join([x.split('\t')[1] for x in lines_val]).encode('utf-8'))
