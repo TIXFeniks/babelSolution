71a371abf8115bcddd935f414ef6bd0a4c952233
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-02-01 22:55:12 +0300

    feat: add exp running script

---
+++ run_experiment.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+EXPERIMENT_NAME=$1
+
+# Fetching the branch
+git reset --hard && git clean -fd
+git fetch --all
+git checkout "$EXPERIMENT_NAME"
+
+# Running experiment
+# 1. Build docker image
+sudo nvidia-docker build -t "universome/$EXPERIMENT_NAME" .
+
+# 2. OPTIONAL: Testing locally on small data
+# 2.1. Creating output dir
+OUTPUT_DIR="/home/user32878/data/outputs/output_$EXPERIMENT_NAME"
+mkdir "$OUTPUT_DIR"
+
+# 2.2 Running experiment locally on small data
+sudo nvidia-docker run -v /home/user32878/data/test_data2/:/input -v "$OUTPUT_DIR":/output -it universome/"$EXPERIMENT_NAME" /nmt/run_lm_fused.sh
+
+# 3. Pushing to docker hub
+sudo docker push "universome/$EXPERIMENT_NAME"
+
+# Let's cat metadata.json which can be copypasted
+METADATA="{\"image\": \"universome/$EXPERIMENT_NAME\", \"entry_point\": \"/nmt/run_lm_fused.sh\"}"
+echo $METADATA
+
+# In case of there are too many docker containers
+# sudo docker stop $(sudo docker ps -a -q) && sudo docker rm $(sudo docker ps -a -q)
+
+# Metadata json should look like this
+# {
+#     "image": "universome/"$EXPERIMENT_NAME"",
+#     "entry_point": "/nmt/run_lm_fused.sh"
+# }
