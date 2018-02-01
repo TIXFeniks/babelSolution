f08375e402713a474fd90e1670d0c08b7bde3d36
TIXFeniks <dimdi-y@ya.ru>
2018-02-01 17:30:04 +0300

    lm full pipeline

--- run_lm_fused.sh
+++ run_lm_fused.sh
@@ -15,7 +15,7 @@ OUTPUT_DATA_PATH="data"
 cd "$PROJECT_DIR"
 
 # Preparing data
-#$PROJECT_DIR/tokenize.sh "$PROJECT_DIR" "$INPUT_DATA_PATH"
+$PROJECT_DIR/tokenize.sh "$PROJECT_DIR" "$INPUT_DATA_PATH"
 
 LANG=1
 MODEL_NAME="lm$LANG"
@@ -25,12 +25,12 @@ MAX_EPOCHS=3
 
 
 # Running the model
-#PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_lm.py" "$MODEL_NAME" \
-#            --data_path="$DATA_PATH" \
-#            --hp_file_path="$HP_FILE_PATH" \
-#            --lang="$LANG" \
-#            --max_epochs=$MAX_EPOCHS \
-#            --max_time_seconds=$MAX_TIME_SECONDS
+PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_lm.py" "$MODEL_NAME" \
+            --data_path="$DATA_PATH" \
+            --hp_file_path="$HP_FILE_PATH" \
+            --lang="$LANG" \
+            --max_epochs=$MAX_EPOCHS \
+            --max_time_seconds=$MAX_TIME_SECONDS
 
 LANG=2
 MODEL_NAME="lm$LANG"
@@ -39,12 +39,12 @@ MAX_TIME_SECONDS=3600
 MAX_EPOCHS=3
 
 # Running the model
-#PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_lm.py" "$MODEL_NAME" \
-#            --data_path="$DATA_PATH" \
-#            --hp_file_path="$HP_FILE_PATH" \
-#            --lang="$LANG" \
-#            --max_epochs=$MAX_EPOCHS \
-#            --max_time_seconds=$MAX_TIME_SECONDS
+PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_lm.py" "$MODEL_NAME" \
+            --data_path="$DATA_PATH" \
+            --hp_file_path="$HP_FILE_PATH" \
+            --lang="$LANG" \
+            --max_epochs=$MAX_EPOCHS \
+            --max_time_seconds=$MAX_TIME_SECONDS
 
 MODEL_NAME="transformer"
 HP_FILE_PATH="$PROJECT_DIR/hp_files/lm_fitted.json"
@@ -56,17 +56,17 @@ USE_EARLY_STOPPING=True
 EARLY_STOPPING_LAST_N=5
 
 # Training the model
-#PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_fused.py" "$MODEL_NAME" \
-#            --data_path="$DATA_PATH" \
-#            --hp_file_path="$HP_FILE_PATH" \
-#            --validate_every="$VALIDATE_EVERY" \
-#            --max_time_seconds="$MAX_TIME_SECONDS" \
-#            --batch_size_for_inference="$BATCH_SIZE_FOR_INFERENCE" \
-#            --use_early_stopping="$USE_EARLY_STOPPING" \
-#            --early_stopping_last_n="$EARLY_STOPPING_LAST_N" \
-#            --max_epochs="$MAX_EPOCHS" \
-#            --target_lm_path="trained_models/lm2/model.npz" \
-#            --src_lm_path="trained_models/lm1/model.npz"
+PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_fused.py" "$MODEL_NAME" \
+            --data_path="$DATA_PATH" \
+            --hp_file_path="$HP_FILE_PATH" \
+            --validate_every="$VALIDATE_EVERY" \
+            --max_time_seconds="$MAX_TIME_SECONDS" \
+            --batch_size_for_inference="$BATCH_SIZE_FOR_INFERENCE" \
+            --use_early_stopping="$USE_EARLY_STOPPING" \
+            --early_stopping_last_n="$EARLY_STOPPING_LAST_N" \
+            --max_epochs="$MAX_EPOCHS" \
+            --target_lm_path="trained_models/lm2/model.npz" \
+            --src_lm_path="trained_models/lm1/model.npz"
 
 # Running the model
 PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/run_fused.py" "$MODEL_NAME" \
