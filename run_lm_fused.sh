8fd60bc6aa1d01e5edc09ddc61c981845d37c18f
antonrykachevskiy <anton.rykachevskiy@yandex.ru>
2018-02-10 22:19:51 +0300

    start

--- run_lm_fused.sh
+++ run_lm_fused.sh
@@ -93,8 +93,8 @@ PYTHONPATH="$PROJECT_DIR" python3.6 "$PROJECT_DIR/src/train_fused.py" "$MODEL_NA
             --max_epochs="$MAX_EPOCHS" \
             --validate_every_epoch="$SHOULD_VALIDATE_EVERY_EPOCH" \
             --warm_up_num_epochs="$WARM_UP_NUM_EPOCHS"
-            #--target_lm_path="$PROJECT_DIR/trained_models/lm2/model.npz" \
-            #--src_lm_path="$PROJECT_DIR/trained_models/lm1/model.npz" \
+            --target_lm_path="$PROJECT_DIR/trained_models/lm2/model.npz" \
+            --src_lm_path="$PROJECT_DIR/trained_models/lm1/model.npz" \
 
 
 ELAPSED_TIME_TRANS_TR=$(($SECONDS - $START_TIME_TRANS_TR))
